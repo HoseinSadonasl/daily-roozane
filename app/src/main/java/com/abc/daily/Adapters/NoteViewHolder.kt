7242a76e8cd2df7a7528b51b1fb2f41a2da078ca
Hoseinsadonasl <hoseinsadonasl@gmail.com>
2023-11-20 00:58:34 +0330

    improve NoteFragment RV and fix some issues

--- app/src/main/java/com/abc/daily/Adapters/NoteViewHolder.kt
+++ app/src/main/java/com/abc/daily/Adapters/NoteViewHolder.kt
@@ -1,8 +1,10 @@
 package com.abc.daily.Adapters
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.abc.daily.databinding.NotesListItemBinding
 import com.abc.daily.domain.model.note.Note
+import com.abc.daily.util.DateUtil
 
 class NoteViewHolder(
     val binding: NotesListItemBinding,
@@ -13,9 +15,17 @@ class NoteViewHolder(
         item.let {
             binding.root.setOnClickListener { item.id?.let { it1 -> onItemClick(it1) } }
             binding.title.text = item.title
-            binding.content.text = item.description
-            binding.textViewDateNotesFragment.text = "textViewDateNotesFragment"
-            binding.reminderDaeTime.text = "reminderDaeTime"
+            if (it.description.isNotBlank()) {
+                binding.content.apply {
+                    visibility = View.VISIBLE
+                    text = it.description
+                }
+            }
+            binding.textViewDateNotesFragment.text = DateUtil.toPersianDateAndTime(item.modifiedAt.toString())
+            it.remindAt?.let {
+                binding.reminderIc.visibility = View.VISIBLE
+                binding.reminderDaeTime.text = DateUtil.toPersianDateAndTime(item.remindAt.toString())
+            }
         }
     }
 }
