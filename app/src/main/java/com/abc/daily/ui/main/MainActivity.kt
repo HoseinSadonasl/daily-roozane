1fac703f61054063d42f18423a141ceb5e0e9cb1
Hoseinsadonasl <hoseinsadonasl@gmail.com>
2023-12-06 01:30:32 +0330

    rmove legacy screen and moving main viewmodel to main activity

--- app/src/main/java/com/abc/daily/ui/main/MainActivity.kt
+++ app/src/main/java/com/abc/daily/ui/main/MainActivity.kt
@@ -2,16 +2,42 @@ package com.abc.daily.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.abc.daily.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+    private var keepSplashOnScreen = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
         setContentView(R.layout.layout_note_fragment)
+        observeData()
+    }
 
+    private fun observeData() {
+        mainViewModel.isDarkMode.observe(this) {isDark ->
+            if (isDark) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        keepSplashOnScreen = false
     }
 }
