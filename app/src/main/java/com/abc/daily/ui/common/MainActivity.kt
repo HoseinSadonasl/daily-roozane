98ce5ab3999247e741ffaf87af37015f66bc19e7
hoseincode <hoseinsadonasl@gmail.com>
2024-01-23 21:38:12 +0330

    fix some issues in request permissin and improve founctionality

--- app/src/main/java/com/abc/daily/ui/common/MainActivity.kt
+++ app/src/main/java/com/abc/daily/ui/common/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.abc.daily.ui.common
 
+import android.Manifest
 import android.app.UiModeManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -18,11 +20,29 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    var resultLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach { permission ->
+                when (permission.key) {
+                    Manifest.permission.ACCESS_COARSE_LOCATION -> {
+                        if (permission.value) commonViewModel.fillLocationPermissionLiveData(permission.value)
+                    }
+                    Manifest.permission.POST_NOTIFICATIONS -> {
+                        if (!permission.value) {
+                            Toast.makeText(this, getString(R.string.toast_notificationpermission), Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+        }
+
     @Inject
     lateinit var uiModeManager: UiModeManager
 
+    @Inject
+    lateinit var commonViewModel: CommonViewModel
+
     private lateinit var binding: LayoutMainBinding
-    private val commonViewModel: CommonViewModel by viewModels()
     private var keepSplashOnScreen = true
     private var isLunched: Boolean = false
 
@@ -70,18 +90,9 @@ class MainActivity : AppCompatActivity() {
         commonViewModel.setThemeColor(themeColor)
     }
 
-    fun requestMultiplePermissions(callBack: (Pair<String, Boolean>) -> Unit) =
-        registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            permissions.entries.forEach { callBack(it.key to it.value) }
-        }
-
     private fun setThemeDarkMode(isNight: Boolean) {
-        val modeUiModeManager =
-            if (isNight) UiModeManager.MODE_NIGHT_YES else UiModeManager.MODE_NIGHT_NO
-        val modeAppCompatDelegate =
-            if (isNight) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        val modeUiModeManager = if (isNight) UiModeManager.MODE_NIGHT_YES else UiModeManager.MODE_NIGHT_NO
+        val modeAppCompatDelegate = if (isNight) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             uiModeManager.setApplicationNightMode(modeUiModeManager)
         } else AppCompatDelegate.setDefaultNightMode(modeAppCompatDelegate)
